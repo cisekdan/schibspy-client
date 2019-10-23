d4fe814596de96124c1b1e699046efb48f28a219
Paweł Karczewski <pawel.karczewski@schibsted.com>
2019-10-23 14:01:13 +0200

    list of winners done

--- schibspy/src/ListOfWinners.js
+++ schibspy/src/ListOfWinners.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import './ListOfWinners.css';
-const ListOfWinners = () => {
-  const winners = ["Kolumb", "Ibisz", "Krawczyk", "Jakiśtam", "Nijaki"];
+import React from "react";
+import "./ListOfWinners.css";
 
-  return(
-    <div className="card">
-      <img height="50" className="medal" src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/google/223/sports-medal_1f3c5.png" />
-      {winners.map(winner => <div className="winner">{winner}</div>)}
+const ListOfWinners = () => {
+  const winners = ["Kolumb", "Ibisz", "Krawczyk", "Jakiśtam", "Nijaki", "Krawczyk", "Jakiśtam", "Nijaki"];
+
+  return (
+    <div className="winners">
+      <div className="winners__card">
+        <div className="winners__img-container">
+          <img height="20"
+               className="winners__medal"
+               src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/google/223/sports-medal_1f3c5.png" />
+        </div>
+        <div className="winners__container">
+          {winners.map((winner, index) => <div key={index}
+                                               className="winners__winner">{winner}</div>)}
+        </div>
+      </div>
     </div>
   );
 };
