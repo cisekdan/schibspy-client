44f5ca5aa5d2fa7a8b9cedbf86365c0b8399fb8b
michalina-kaniewska <michalina.kaniewska@schibsted.com>
2019-10-23 19:16:13 +0200

    Fix video position

--- schibspy/src/Quiz.js
+++ schibspy/src/Quiz.js
@@ -9,9 +9,11 @@ function Quiz () {
 
        return (
            <React.Fragment>
-           <video className="video" autoPlay>
-               <source src="./pytanie_01.mp4" type="video/mp4"/>
-           </video>
+               <div className="video-container">
+                   <video className="video" autoPlay>
+                       <source src="./pytanie_01.mp4" type="video/mp4"/>
+                   </video>
+               </div>
            <div className="quiz">
                <h3 className="quiz__question-number">{questionNumber}/12</h3>
                <div className={`quiz__timer ${secondsLeft <= 3 ? "quiz__timer--ending" : ""}`}><p>{secondsLeft}</p></div>
