74f7ef046bee48c2c66a9615cc663807c346a2c1
michalina-kaniewska <michalina.kaniewska@schibsted.com>
2019-10-23 14:48:54 +0200

    Showing results

--- schibspy/src/Quiz.js
+++ schibspy/src/Quiz.js
@@ -6,20 +6,18 @@ function Quiz() {
 
     const questionNumber = 2;
     const questionTitle = "Amerykę odkrył Krzysztof:";
-    const answers = ["Kolumb", "Ibisz", "Krawczyk"];
-    const secondsLeft = 5;
-
-
+    const answers = [{id: 0, value: "Kolumb"}, {id: 1, value: "Ibisz"}, {id: 2, value: "Krawczyk"}];
+    const secondsLeft = 1;
 
     return (
         <div className="quiz">
             <h3 className="quiz__question-number">Pytanie {questionNumber}</h3>
-            <div className="quiz__timer"><p>{secondsLeft}</p></div>
+            <div className={`quiz__timer ${secondsLeft <= 3 ? "quiz__timer--ending" : ""}`}><p>{secondsLeft}</p></div>
             <div className="quiz__question-panel">
                 <div className="quiz__question-panel__title">
                     {questionTitle}
                 </div>
-                {<Answers answers={answers}/>}
+                {<Answers answers={answers} secondsLeft={secondsLeft}/>}
             </div>
         </div>
     );
