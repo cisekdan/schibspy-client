9815c0f26d8f08c85d9b2a53c1f330f5fe08228e
Mickanie <45340483+Mickanie@users.noreply.github.com>
2019-12-01 18:04:13 +0100

    Hidden question mode & quiz header (#15)

---
+++ src/components/Quiz/Header.js
@@ -0,0 +1,17 @@
+import React, {useContext} from 'react';
+import {QuizContainer} from '../../QuizContainer';
+
+const Header = () => {
+    const {questionNumber, questionCount, player} = useContext(QuizContainer.QuizContext);
+    return (
+        <div className="quiz__header">
+            <h3 className="quiz__header__question-number">{questionNumber !== 0 ? `${questionNumber}/${questionCount}` : ""}</h3>
+            <div className="quiz__header__player-info">
+                <h3>{player.name}</h3>
+                <img width="50px" height="50px" src={player.avatarUrl}/>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/components/Quiz/index.js
@@ -0,0 +1,56 @@
+import React, { useContext } from "react";
+import "../../scss/Quiz.scss";
+import Answers from "./Answers";
+import { QuizContainer } from "../../QuizContainer";
+import YouTube from 'react-youtube';
+import Header from "./Header";
+
+const videoOptions = {
+  playerVars: { // https://developers.google.com/youtube/player_parameters
+    autoplay: 1,
+    controls: 0,
+    rel: 0,
+    showinfo: 0
+  }
+};
+
+const Quiz = () => {
+
+  const {question, questionNumber, youTubeId} = useContext(QuizContainer.QuizContext);
+  const hideQuestionPanel = questionNumber === 0 || question.status === "hidden";
+  const {seconds_left: secondsLeft, title} = question;
+
+  const _onReady = (event) => {
+    event.target.playVideo();
+  };
+
+  const _onEnd = (event) => {
+    event.target.stopVideo();
+  };
+
+  return (
+      <div className="quiz">
+        <div className="video-background">
+          <div className="video-foreground">
+            <YouTube
+                videoId={youTubeId}
+                opts={videoOptions}
+                className="video-iframe"
+                onReady={_onReady}
+                onEnd={_onEnd}
+            />
+          </div>
+        </div>
+        <Header/>
+        {!hideQuestionPanel &&
+          <div className="quiz__question-panel">
+            <div className={`quiz__question-panel__timer ${secondsLeft <= 3 ? "quiz__timer--ending" : ""}`}><p>{secondsLeft}</p></div>
+            <h3 className="quiz__question-panel__title">{title}</h3>
+            <Answers/>
+          </div>}
+      </div>
+  );
+}
+
+export default Quiz;
+
