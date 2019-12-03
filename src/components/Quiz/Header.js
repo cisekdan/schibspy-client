63ca8cf6a4b4174a12072dd66f5376ab1a838a7b
michalina-kaniewska <michalina.kaniewska@schibsted.com>
2019-12-03 11:27:54 +0100

    Add player score to quiz header

--- src/components/Quiz/Header.js
+++ src/components/Quiz/Header.js
@@ -6,6 +6,7 @@ const Header = () => {
     return (
         <div className="quiz__header">
             <h3 className="quiz__header__question-number">{questionNumber !== 0 ? `${questionNumber}/${questionCount}` : ""}</h3>
+            <h3 className="quiz__header__score">Wynik: {player.score}</h3>
             <div className="quiz__header__player-info">
                 <h3>{player.name}</h3>
                 <img width="50px" height="50px" src={player.avatarUrl}/>
