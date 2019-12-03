0ecbe1451eb236ea0e8fe1cb2486c94cfe1ed2c7
michalina-kaniewska <michalina.kaniewska@schibsted.com>
2019-12-03 20:44:53 +0100

    Change displaying question number logic

--- src/components/Quiz/Header.js
+++ src/components/Quiz/Header.js
@@ -5,7 +5,8 @@ const Header = () => {
     const {questionNumber, questionCount, player} = useContext(QuizContainer.QuizContext);
     return (
         <div className="quiz__header">
-            <h3 className="quiz__header__question-number">{questionNumber !== 0 ? `${questionNumber}/${questionCount}` : ""}</h3>
+            {questionNumber !==0 && <h3 className="quiz__header__question-number">{questionNumber}/{questionCount}</h3>}
+            <h3 className="quiz__header__score">Wynik: {player.score}</h3>
             <div className="quiz__header__player-info">
                 <h3>{player.name}</h3>
                 <img width="50px" height="50px" src={player.avatarUrl}/>
