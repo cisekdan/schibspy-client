a2407f0e93e174b5c8964c066fd7a43e1882ee17
Mickanie <45340483+Mickanie@users.noreply.github.com>
2019-12-05 15:03:36 +0100

    Spectator mode when quiz has already started (#18)

--- src/components/Quiz/Header.js
+++ src/components/Quiz/Header.js
@@ -5,12 +5,16 @@ const Header = () => {
     const {questionNumber, questionCount, player} = useContext(QuizContainer.QuizContext);
     return (
         <div className="quiz__header">
-            {questionNumber !==0 && <h3 className="quiz__header__question-number">{questionNumber}/{questionCount}</h3>}
-            <h3 className="quiz__header__score">Wynik: {player.score}</h3>
-            <div className="quiz__header__player-info">
-                <h3>{player.name}</h3>
-                <img width="50px" height="50px" src={player.avatarUrl}/>
-            </div>
+            {questionNumber !== 0 &&
+            <h3 className="quiz__header__question-number">{questionNumber}/{questionCount}</h3>}
+            {player.mode === "player" &&
+            <>
+                <h3 className="quiz__header__score">Wynik: {player.score}</h3>
+                <div className="quiz__header__player-info">
+                    <h3>{player.name}</h3>
+                    <img width="50px" height="50px" src={player.avatarUrl}/>
+                </div>
+            </>}
         </div>
     )
 }
