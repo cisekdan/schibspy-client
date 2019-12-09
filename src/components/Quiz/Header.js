a118b5d80a41cf7b63765ef80ef0cb8f5658dcc5
michalina-kaniewska <michalina.kaniewska@schibsted.com>
2019-12-09 23:46:41 +0100

    Fix header font sizes

--- src/components/Quiz/Header.js
+++ src/components/Quiz/Header.js
@@ -9,8 +9,8 @@ const Header = () => {
             <h3 className="quiz__header__question-number">{questionNumber}/{questionCount}</h3>}
             {player.mode === "player" &&
             <>
-                <h3 className="quiz__header__score">Wynik: {player.score}</h3>
-                <h3 className="quiz__header__player-name">{player.name} <img alt="Player avatat" width="45px" height="45px" src={player.avatarUrl}/></h3>
+                <h3 className="quiz__header__score">Wynik:{player.score}</h3>
+                <h3 className="quiz__header__player-name">{player.name} <img alt="Player avatar" width="45px" height="45px" src={player.avatarUrl}/></h3>
             </>}
         </div>
     )
