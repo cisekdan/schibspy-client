e63f91c20bf803016591c08b6e6914bb2bad8355
Paweł Karczewski <paw.karczewski@gmail.com>
2019-12-12 20:12:14 +0100

    Password input (#28)

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -3,25 +3,28 @@ import "../scss/Registration.scss";
 import { QuizContext } from "../QuizContext";
 
 const Registration = () => {
-  const {setRegisteredUser, registeredUser, player, generateNewAvatar, switchToFullScreen} = useContext(QuizContext);
+  const {setRegisteredUser, registeredUser, player, generateNewAvatar, switchToFullScreen, setPassword, showAlert} = useContext(QuizContext);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     switchToFullScreen();
     setRegisteredUser(e.target.name.value);
-
+    setPassword(e.target.password.value);
   };
 
   return (
-    <div className="registration">
-      <h2>Zaraz zaczynamy</h2>
-      <img alt="player avatar" className={"registration__avatar"} src={player.avatarUrl} />
-      {registeredUser ? <h3>{registeredUser}</h3> : <>
-        <button className="registration__shuffle-button"
-                onClick={generateNewAvatar}>Wylosuj nowy avatar
-        </button>
-        <Form handleSubmit={handleSubmit} /> </>}
-    </div>
+    <>
+      {showAlert ? <div className="quiz__overlay__alert">Nieprawidłowe hasło.</div> : <div className="registration">
+        <h2>Zaraz zaczynamy</h2>
+        <img alt="player avatar"
+             className={"registration__avatar"}
+             src={player.avatarUrl} />
+        {registeredUser ? <h3>{registeredUser}</h3> : <>
+          <button className="registration__shuffle-button"
+                  onClick={generateNewAvatar}>Wylosuj nowy avatar
+          </button>
+          <Form handleSubmit={handleSubmit} /> </>}</div>}
+    </>
   );
 };
 
@@ -40,7 +43,18 @@ const Form = ({handleSubmit}) => {
                className="form__input"
                type="text"
                name="name"
-               maxLength={20}/>
+               maxLength={20} />
+      </div>
+      <div className="form__form-group">
+        <label htmlFor="password-input"
+               className="form__label">
+          Hasło
+        </label>
+        <input id="password-input"
+               className="form__input"
+               type="text"
+               name="password"
+               maxLength={20} />
       </div>
       <button className="form__submit"
               type="submit">Gramy!
