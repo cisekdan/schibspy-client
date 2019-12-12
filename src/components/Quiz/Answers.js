ffe1831c3de25972c578b4c26efa469303e55bb2
michalina-kaniewska <michalina.kaniewska@schibsted.com>
2019-12-12 20:04:30 +0100

    Remove mocks

--- src/components/Quiz/Answers.js
+++ src/components/Quiz/Answers.js
@@ -2,28 +2,9 @@ import React, {useContext} from "react";
 import {QuizContainer} from "../../QuizContainer";
 
 function Answers() {
-    const {question, sendChosenAnswer, player, setShowAlert, switchToFullScreen} = useContext(QuizContainer.QuizContext);
+    const {question, sendChosenAnswer, player, setShowAlert} = useContext(QuizContainer.QuizContext);
     const {answers, seconds_left: secondsLeft, status: questionStatus, correct_answer: correctAnswer, total_responses: totalResponses} = question;
     const playersAnswer = player.answers ? player.answers[question.id] : null;
-
-    const answersMock = [
-        {id: "001d0bba-8fbe-4557-8028-29e0b01897aa",
-            body: "Fale Dunaju", position: 0, answer_count: 8,
-            players: [{ id: "VMP85qSKT16qzCsDAA0", name: "ksjklgjflksjlkf", avatarUrl: "https://schibspy-server.herokuapp.com/avatars/102.svg"},
-                { id: "VMP85qSKT16qzCsDAA1_", name: "ksjklgjflksjlkf", avatarUrl: "https://schibspy-server.herokuapp.com/avatars/102.svg"},
-                { id: "VMP85qSKT16qzCsDAA2_", name: "ksjklgjflksjlkf", avatarUrl: "https://schibspy-server.herokuapp.com/avatars/103.svg"},
-                { id: "VMP85qSKT16qzCsDAA3_", name: "ksjklgjflksjlkf", avatarUrl: "https://schibspy-server.herokuapp.com/avatars/104.svg"},
-                { id: "VMP85qSKT16qzCsDAA4_", name: "ksjklgjflksjlkf", avatarUrl: "https://schibspy-server.herokuapp.com/avatars/105.svg"},
-                { id: "VMP85qSKT16qzCsDAA5_", name: "ksjklgjflksjlkf", avatarUrl: "https://schibspy-server.herokuapp.com/avatars/106.svg"},
-                { id: "VMP85qSKT16qzCsDAA6_", name: "ksjklgjflksjlkf", avatarUrl: "https://schibspy-server.herokuapp.com/avatars/107.svg"},
-                { id: "VMP85qSKT16qzCsDAA7_", name: "ksjklgjflksjlkf", avatarUrl: "https://schibspy-server.herokuapp.com/avatars/108.svg"},
-                { id: "VMP85qSKT16qzCsDAA8_", name: "ksjklgjflksjlkf", avatarUrl: "https://schibspy-server.herokuapp.com/avatars/109.svg"}
-        ]},
-        {id: "8f35c87d-a283-4a03-839c-abf323d34806", body: "Fale radiowe", position: 2, answer_count: 0, players: Array(0)},
-        {id: "ee5397eb-32ac-4cc9-923a-942d74e183bb", body: "Falafele", position: 1, answer_count: 0, players: Array(0)}
-    ];
-
-
 
     const chooseAnswer = (id) => {
         if(player.mode !== "player"){
@@ -97,7 +78,7 @@ function Answers() {
                      className={`answers__answer__inner ${setAnswerColors(answers[0].id)}`}>
                 </div>
                 <p className="answers__answer__text"> {answers[0].body}</p>
-                <div className="answers__answer__count">{displayAvatars(answersMock[0])}</div>
+                <div className="answers__answer__count">{displayAvatars(answers[0])}</div>
             </div>
             <div className="answers__answer">
                 <div style={{width: `${setBarWidth(answers[1])}%`}}
