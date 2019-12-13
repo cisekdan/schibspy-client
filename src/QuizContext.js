b74c5c8349b441578cf8fb37fd65fec905b0478a
michalina-kaniewska <michalina.kaniewska@schibsted.com>
2019-12-13 18:50:34 +0100

    Longer Slasher

--- src/QuizContext.js
+++ src/QuizContext.js
@@ -53,7 +53,7 @@ export function initContextValue () {
     socket.on("slasher", () => {
       setTimeout(() => {
         setShowSlasher(true);
-        setTimeout(() => setShowSlasher(false), 9500);
+        setTimeout(() => setShowSlasher(false), 12000);
       }, 3000);
     });
     socket.on("winners_list", (winnersList) => {
